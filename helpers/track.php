76c99a4431c7918dbb5ceeab7e2ed0792c854f67
Christoph Dittmann <github@yozora.eu>
2024-05-20 11:55:00 +0200

    Order track list by name, assuming they have an ISO date prefix

--- helpers/track.php
+++ helpers/track.php
@@ -211,7 +211,7 @@ class uTrack {
     } else {
       $where = "";
     }
-    $query = "SELECT id, user_id, name, comment FROM " . self::db()->table('tracks') . " $where ORDER BY id DESC";
+    $query = "SELECT id, user_id, name, comment FROM " . self::db()->table('tracks') . " $where ORDER BY name DESC";
     try {
       $result = self::db()->query($query);
       $trackArr = [];
